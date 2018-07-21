5980efc936bafbd78a56cd786e38f6f59391d3bf
Roberto Garza <robertg4567@hotmail.com>
2018-07-21 17:27:32 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
 def prime?(myInt)
-  myPrimeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131]
-  if myPrimeNumbers.include?(myInt)
-    true
+  if myInt < 0 or myInt == 0 or myInt == 1
+    return false
   else
-    false
+    (2..myInt-1).to_a.all? do |factor|
+      myint % factor != 0
+    end
   end
-
-
 end
