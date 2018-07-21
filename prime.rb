19f9a1c2470f93aa32a292ad01ac4aa26fab8f3c
Roberto Garza <robertg4567@hotmail.com>
2018-07-21 17:23:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,6 @@
 def prime?(myInt)
-  myPrimeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131]
-  if myPrimeNumbers.include?(myInt)
-    return true
-  else
+  if myInt == 0 || myInt < 0 || myInt == 1
     return false
   end
 end
+ 
